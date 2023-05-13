b1195aaa16d24a325a62ad9727fc4c4cc3cbcd98
Allen Zheng <volcaronitee8533@gmail.com>
2023-05-13 13:27:36 -0400

    Frontend Complete

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { useState, useEffect } from "react";
+
+function App() {
+  const [listOfUsers, setListOfUsers] = useState([]);
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
---
+++ server/models/Users.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+});
+
+const UserModel = mongoose.model("users", UserSchema);
+module.exports = UserModel;
