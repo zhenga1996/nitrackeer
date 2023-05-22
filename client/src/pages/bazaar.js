b2759dcf0099f26917fc66401af388fe37edaa8f
Allen Zheng <volcaronitee8533@gmail.com>
2023-05-22 12:12:04 -0400

    Search bar complete

--- client/src/pages/bazaar.js
+++ client/src/pages/bazaar.js
@@ -2,22 +2,26 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { toProperCase, nFormatter } from "../functions";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faList, faGrip } from '@fortawesome/free-solid-svg-icons'
+import { faList, faGrip, faSearch } from '@fortawesome/free-solid-svg-icons'
 import { Loading } from "./loading";
 
 export const Bazaar = () => {
     const [items, setItems] = useState([]);
+    const [filtered, setFiltered] = useState("");
+    const [search, setSearch] = useState("");
+    const [view, setView] = useState("list-view");
 
+    // List control
     useEffect(() => {
         axios.get('https://api.slothpixel.me/api/skyblock/bazaar')
         .then(res => {
             // Fomatting as array since I keep getting an error when using whole dictionary
-            // Array indexes: [product_id, ]
+            // Array indexes: [name, product_id, sellPrice, sellMovingWeek, buyPrice, buyMovingWeek]
             let list = [];
             for (const [key, value] of Object.entries(res.data)) {
                 const stats = value.quick_status;
                 const id = value.name ? value.name : toProperCase(stats.productId);
-                if (!key.includes("ENCHANTMENT")) {
+                if (stats.buyMovingWeek) {
                     list.push([
                         id,
                         stats.productId,
@@ -29,38 +33,25 @@ export const Bazaar = () => {
                 }
             }
             setItems(list);
+            setFiltered(list);
         })
         .catch(err => console.log(err));
     }, [])
 
-    // List / Grid Control
-    var li_links = document.querySelectorAll(".links ul li");
-    var view_wraps = document.querySelectorAll(".view_wrap");
-    var list_view = document.querySelector(".list-view");
-    var grid_view = document.querySelector(".grid-view");
-
-    li_links.forEach(function(link){
-        link.addEventListener("click", function(){
-            li_links.forEach(function(link){
-                link.classList.remove("active");
-            })
-
-            link.classList.add("active");
-
-            var li_view = link.getAttribute("data-view");
-
-            view_wraps.forEach(function(view){
-                view.style.display = "none";
-            })
-
-            if(li_view == "list-view"){
-                list_view.style.display = "block";
-            }
-            else{
-                grid_view.style.display = "block";
-            }
-        })
-    })
+    // Control search bar
+    const searchChange = (event) => {
+        setSearch(event.target.value);
+    };
+    const searchSubmit = (event) => {
+        event.preventDefault();
+        setFiltered(items.filter(item => item[0].toLowerCase().includes(search.toLowerCase())));
+    };
+    
+    const imageOnError = (event) => {
+        event.currentTarget.src = "https://sky.lea.moe/item/BOOK";
+        event.currentTarget.className = "error";
+        event.currentTarget.onError = null;
+    };
 
     if (!items.length) return <Loading />;
 
@@ -68,47 +59,46 @@ export const Bazaar = () => {
     <div className="wrapper">
         <div className="links">
             <ul>
-                <li className="li-list" data-view="list-view">
+                <li className="li-list" onClick={ () => setView("list-view") }>
                     <FontAwesomeIcon icon={ faList }/> List View</li>
-                <li className="li-grid">
+                <li className="li-grid" onClick={ () => setView("grid-view") }>
                     <FontAwesomeIcon icon={ faGrip }/> Grid View</li>
             </ul>
         </div>
 
+        <form onSubmit={ searchSubmit }>
+            <label><FontAwesomeIcon icon={ faSearch }/>&ensp;</label>
+            <input type="text" value={search} onChange={ searchChange }/>
+            <input className="search" type="submit" value="Search" />
+        </form>
+
         <div className="view_main">
-            <div className="view_wrap list-view">
-                { items.map((item) => {
-                    return <div className="view_item">
+            <div className={ "view_wrap " + view }>
+                { filtered.map((item) => {
+                    return (<div className="view_item" key={ item[1] }>
                         <div className="vi_left">
-                            <img src={ "https://sky.lea.moe/item/" + item[1] } alt="product"/>
+                            <img src={ "https://sky.lea.moe/item/" + item[1] } alt="product" onError={ imageOnError }/>
                         </div>
                         <div className="vi_right">  
                             <p className="title">{ item[0] }</p>
-                            <p className="content">
-                                <strong> Insta Sell: </strong> { item[2] } coins&emsp;&ensp;
-                                <strong> Weekly Sell: </strong>{ item[3] }&emsp;&ensp;
-                                <strong> Insta Buy: </strong>{ item[4] } coins&emsp;&ensp;
-                                <strong> Weekly Buy: </strong>{ item[5] }</p>
+                            {
+                                view === "list-view" ?
+                                <p className="content">
+                                    <strong> Insta Sell: </strong> { item[2] } coins&emsp;&ensp;
+                                    <strong> Weekly Sell: </strong>{ item[3] }&emsp;&ensp;
+                                    <strong> Insta Buy: </strong>{ item[4] } coins&emsp;&ensp;
+                                    <strong> Weekly Buy: </strong>{ item[5] }
+                                </p> :
+                                <p className="content">
+                                    <p className="content">Insta Sell Price: { item[2] } coins</p>
+                                    <p className="content">Weekly Insta Sell: { item[3] }</p>
+                                    <p className="content">Insta Buy Price: { item[4] } coins</p>
+                                    <p className="content">Weekly Insta Buy: { item[5] }</p>
+                                </p>
+                            }
                             <div className="btn">View More</div>
                         </div>
-                    </div>
-                })}
-            </div>
-            <div className="view_wrap grid-view">
-                { items.map((item) => {
-                    return <div className="view_item">
-                        <div className="vi_left">
-                            <img src={ "https://sky.lea.moe/item/" + item[1] } alt="product"/>
-                        </div>
-                        <div className="vi_right">
-                            <p className="title">{ item[0] }</p>
-                            <p className="content">Insta Sell Price: { item[2] } coins</p>
-                            <p className="content">Weekly Insta Sell: { item[3] }</p>
-                            <p className="content">Insta Buy Price: { item[4] } coins</p>
-                            <p className="content">Weekly Insta Buy: { item[5] }</p>
-                            <div className="btn">View More</div>
-                        </div>
-                    </div>
+                    </div>);
                 })}
             </div>
         </div>
